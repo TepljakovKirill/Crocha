0621145862303363887501321e6fc776957f8e46
Kirill Tepljakov <super.css2017@yandex.ru>
2022-11-08 16:27:51 +0300

    end 1 part

--- src/Components/Slider/slider.js
+++ src/Components/Slider/slider.js
@@ -3,34 +3,21 @@ import "./slider.css";
 function Slider() {
   return (
     <div className="container">
-      <h1>Акции</h1>
-      <div className="slider flex">
-        <div className="cart">
-          <img src="./img/card-1.jpg" alt="" />
-          <div className="cart-content">
-            <p className="cart-title">Костюм для девочки</p>
-            <div className="cart-price flex">
-              <p className="cart-price__total">559 ₽</p>
-              <p className="cart-price__old">719 ₽</p>
-            </div>
-            <div className="cart-button flex">
-              <button className="button-watch">Смотреть</button>
-              <button className="button-cart">В корзину</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="cart">
-          <img src="./img/card-1.jpg" alt="" />
-          <div className="cart-content">
-            <p className="cart-title">Костюм для девочки</p>
-            <div className="cart-price flex">
-              <p className="cart-price__total">559 ₽</p>
-              <p className="cart-price__old">719 ₽</p>
-            </div>
-            <div className="cart-button flex">
-              <button className="button-watch">Смотреть</button>
-              <button className="button-cart">В корзину</button>
+      <div className="slider-container">
+        <h1>Акции</h1>
+        <div className="slider flex">
+          <div className="cart">
+            <img src="./img/card-1.jpg" alt="" />
+            <div className="cart-content">
+              <p className="cart-title">Костюм для девочки</p>
+              <div className="cart-price flex">
+                <p className="cart-price__total">559 ₽</p>
+                <p className="cart-price__old">719 ₽</p>
+              </div>
+              <div className="cart-button flex">
+                <button className="button-watch">Смотреть</button>
+                <button className="button-cart">В корзину</button>
+              </div>
             </div>
           </div>
         </div>
