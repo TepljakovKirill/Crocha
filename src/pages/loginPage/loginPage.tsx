ed98353efeca99b1835e05debf2588dcf75993c4
Kirill Tepljakov <super.css2017@yandex.ru>
2023-12-12 14:49:36 +0300

    заверстал страницу регистрации

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -6,7 +6,59 @@ function LoginPage() {
       <Helmet>
         <title>Crocha - Страница регистрации</title>
       </Helmet>
-      <div>Страница Регистрации</div>;
+      <div className="container">
+        <div className="authorization flex">
+          <p className="authorization-title">
+            Для возможности заказа товаров в нашем интернет-магазине необходима
+            регистрация:)
+          </p>
+          <form className="flex" onSubmit="onSubmit">
+            <div className="field">
+              <div>
+                <label htmlFor="email">Полное имя:</label>
+              </div>
+              <input
+                type="text"
+                placeholder="Антонина Семёновна"
+                name="email"
+                required
+              />
+            </div>
+
+            <div className="field">
+              <div>
+                <label htmlFor="password">Почта:</label>
+              </div>
+              <input
+                placeholder="newuser@mokky.test"
+                type="email"
+                id="email"
+                required
+              />
+            </div>
+
+            <div className="field">
+              <div>
+                <label htmlFor="password">Пароль:</label>
+              </div>
+              <input
+                placeholder="123456"
+                type="password"
+                id="password"
+                required
+              />
+            </div>
+
+            <button className="button-blue" type="submit">
+              Зарегистрироваться
+            </button>
+          </form>
+
+          <h2 id="status" className="error">
+            Не зарегистрирован!
+          </h2>
+        </div>
+      </div>
     </>
   );
 }
