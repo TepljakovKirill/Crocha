73164c2a3b90f74998e968abbeec709b18a60d7b
Kirill Tepljakov <super.css2017@yandex.ru>
2024-01-04 17:41:14 +0300

    мелкое форматирование

--- src/components/basketCard/basketCard.tsx
+++ src/components/basketCard/basketCard.tsx
@@ -6,49 +6,51 @@ import { deleteCard } from "../../redux/slices/cartSlice";
 import { useDispatch } from "react-redux";
 
 type TProductProps = {
-    id: number,
-    imageUrl: string,
-    title: string,
-    price: number,
-    oldPrice: number
-}
+  id: number;
+  imageUrl: string;
+  title: string;
+  price: number;
+  oldPrice: number;
+};
 
 type TBasketCartProps = {
-    product: TProductProps
-}
-
-
+  product: TProductProps;
+};
 
 function BasketCard({ product }: TBasketCartProps) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const removeCard = (id: number) => {
-        dispatch(deleteCard(id));
-        dispatch(setTotalSumDeleteProduct(price));
-        dispatch(setDeleteDiscount(price));
-    }
+  const removeCard = (id: number) => {
+    dispatch(deleteCard(id));
+    dispatch(setTotalSumDeleteProduct(price));
+    dispatch(setDeleteDiscount(price));
+  };
 
-    const { id, imageUrl, title, price, oldPrice } = product;
+  const { id, imageUrl, title, price, oldPrice } = product;
 
-    return (
-        <div className="basket-total__card flex">
-            <div className="card-wrap flex">
-                <img src={imageUrl} alt="Фото товара" />
-                <div className="card-descr flex">
-                    <p className="card-title">{title}</p>
-                    <div className="card-price__block flex">
-                        <p className="card-price__new">{price} ₽</p>
-                        {
-                            oldPrice > 0 ? <p className="card-price__old">{oldPrice} ₽</p> : " "
-                        }
-                    </div>
-                    <p className="card-weight">Вес посылки 400 г.</p>
-                </div>
-            </div>
-            <CounterProduct />
-            <button onClick={() => removeCard(id)} className="basket-button">&times;</button>
+  return (
+    <div className="basket-total__card flex">
+      <div className="card-wrap flex">
+        <img src={imageUrl} alt="Фото товара" />
+        <div className="card-descr flex">
+          <p className="card-title">{title}</p>
+          <div className="card-price__block flex">
+            <p className="card-price__new">{price} ₽</p>
+            {oldPrice > 0 ? (
+              <p className="card-price__old">{oldPrice} ₽</p>
+            ) : (
+              " "
+            )}
+          </div>
+          <p className="card-weight">Вес посылки 400 г.</p>
         </div>
-    );
+      </div>
+      <CounterProduct />
+      <button onClick={() => removeCard(id)} className="basket-button">
+        &times;
+      </button>
+    </div>
+  );
 }
 
 export default BasketCard;
--- src/redux/slices/cartSlice.tsx
+++ src/redux/slices/cartSlice.tsx
@@ -1,47 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 type CartsState = {
-    carts: number[],
-    discount: number,
-    totalSumProduct: number,
-    totalSumOrder: number
-}
+  carts: number[];
+  discount: number;
+  totalSumProduct: number;
+  totalSumOrder: number;
+};
 
 const initialState: CartsState = {
-    carts: [],
-    discount: 0,
-    totalSumProduct: 0,
-    totalSumOrder: 0
-
-}
+  carts: [],
+  discount: 0,
+  totalSumProduct: 0,
+  totalSumOrder: 0,
+};
 
 const cartSlice = createSlice({
-    name: 'carts',
-    initialState,
-    reducers: {
-        setCarts(state, action) {
-            state.carts.push(action.payload);
-        },
-        deleteCard(state, action) {
-            state.carts = state.carts.filter(cart => cart.id !== action.payload)
-        },
-        setTotalSumProduct(state, action) {
-            state.totalSumProduct += action.payload;
-        },
-        setTotalSumDeleteProduct(state, action) {
-            state.totalSumProduct -= action.payload;
-        },
-        setDiscount(state, action) {
-            state.discount += action.payload;
-        },
-        setDeleteDiscount(state, action) {
-            state.discount -= action.payload;
-        },
-    }
-})
+  name: "carts",
+  initialState,
+  reducers: {
+    setCarts(state, action) {
+      state.carts.push(action.payload);
+    },
+    deleteCard(state, action) {
+      state.carts = state.carts.filter((cart) => cart.id !== action.payload);
+    },
+    setTotalSumProduct(state, action) {
+      state.totalSumProduct += action.payload;
+    },
+    setTotalSumDeleteProduct(state, action) {
+      state.totalSumProduct -= action.payload;
+    },
+    setDiscount(state, action) {
+      state.discount += action.payload;
+    },
+    setDeleteDiscount(state, action) {
+      state.discount -= action.payload;
+    },
+  },
+});
 
-
-
-export const { setCarts, deleteCard, setTotalSumProduct, setTotalSumDeleteProduct, setDiscount, setDeleteDiscount } = cartSlice.actions;
+export const {
+  setCarts,
+  deleteCard,
+  setTotalSumProduct,
+  setTotalSumDeleteProduct,
+  setDiscount,
+  setDeleteDiscount,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
